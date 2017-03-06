145dd365d9e31e01bc43a306af7b6cd8e2e920b3
Andrew <andrewnyash@gmail.com>
2017-03-07 00:21:45 +0300

    Gulpfile rewrite

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-var gulp = require('gulp'),
+"use strict";
+let gulp = require('gulp'),
     less = require('gulp-less'),
     path = require('path'),
+    notify = require('gulp-notify'),
+    util = require('gulp-util'),
+    cssmin = require('gulp-cssmin'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    babel = require('gulp-babel'),
     LessAutoprefix = require('less-plugin-autoprefix'),
     autoprefix = new LessAutoprefix({
         browsers: ['last 2 versions']
@@ -9,48 +16,84 @@ var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     browserSync = require('browser-sync'),
     imagemin = require('gulp-imagemin'),
-    reload = browserSync.reload;
-//var lr = require('tiny-lr');
-//var server = lr();
+    reload = browserSync.reload,
+    exec = require('child_process').exec,
+    plumberErrorHandler = {
+        errorHandler: notify.onError({
+            title: 'Gulp',
+            message: 'Error: <%= error.message %>'
+        })
+    },
+    config = {
+        production: !!util.env.production,
+        srcDir: 'src/',
+        distDir: 'dist/'
+    }
 
-gulp.task('browserSync', function() {
+gulp.task('runserver', function() {
+    let proc = exec('python3 manage.py runserver')
+})
+
+
+gulp.task('browserSync', ['runserver'], function() {
     browserSync({
-        server: {
-            baseDir: "./"
-        },
-        port: 8080,
+        port: 8000,
         open: true,
+        proxy: 'localhost:8000',
         notify: false
     });
 });
 
-gulp.task('styles', function() {
-    return gulp.src(['src/less/main.less'])
-        .pipe(
-            less({
-                plugins: [autoprefix]
-            })
-        ).pipe(gulp.dest('dist/css'))
-        .pipe(reload());
+gulp.task('production:styles', function() {
+    return gulp.src('src/less/main.less')
+        .pipe(sourcemaps.init())
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(less({
+            plugins: [autoprefix]
+        }))
+        .pipe(cssmin())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/css'));
 });
 
-// gulp.task('img', function() {
-//     return gulp.src(['src/img/*.{png,jpg,gif}'])
-//         .pipe(imagemin({
-//
-//             optimizationLevel: 7,
-//
-//             progressive: true
-//
-//         }))
-//         .pipe(gulp.dest('dist/img'));
-// });
+gulp.task('scripts', function() {
+    gulp.src('src/js/*.js')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(babel())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(reload({
+            stream: true
+        }))
+});
+
+gulp.task('img', function() {
+    return gulp.src(['src/img/*.{png,jpg,gif}'])
+        .pipe(imagemin({
+            optimizationLevel: 7,
+            progressive: true
+
+        }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('scripts', function() {
+    gulp.src('vendor/src/js/*.js')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(babel())
+        .pipe(gulp.dest('vendor/assets/javascripts'))
+        .pipe(reload({
+            stream: true
+        }))
+});
 
 gulp.task('watch', function() {
     gulp.watch(['src/img/*.{png,jpg,gif}'], ['img']);
+    gulp.watch(['src/js/*.js'], ['scripts']);
     gulp.watch(['*.html', 'src/less/*.less']).on('change', function(evt) {
         browserSync.reload();
     });
 });
-
-gulp.task('default', ['watch', 'browserSync']);
+if (config.production) {
+    gulp.task('default', ['scripts', 'img', 'production:styles']);
+} else
+    gulp.task('default', ['scripts', 'img', 'production:styles', 'watch', 'browserSync']);
