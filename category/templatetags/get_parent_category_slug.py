ce0a1061b3b571e3602c18f972a1915600afd505
Andrew <andrewnyash@gmail.com>
2017-03-20 15:17:44 +0300

    Child categories

---
+++ category/templatetags/get_parent_category_slug.py
@@ -0,0 +1,9 @@
+from django.template.defaulttags import register
+from category.models import Category
+
+
+@register.filter
+def get_parent_category_slug(cat_id):
+    category = Category.objects.get(id=cat_id)
+    parent_category = Category.objects.get(id=category.parent_category_id)
+    return parent_category.category.slug
